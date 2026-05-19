f60ba1fe26299629c19a173432f2b10bfdad450d
pauloojr <paulojr7704@outlook.com>
2026-05-19 12:12:02 -0300

    teste 2

--- infra/main.tf
+++ infra/main.tf
@@ -3,102 +3,105 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azure_virtual_network" "vnet" {
-  name = "vnet-automation"
-  address_space = ["10.0.0.0/16"]
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.name
+# CORRIGIDO: Era azure_virtual_network, o correto é azurerm_virtual_network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-automation"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  # CORRIGIDO: Faltava o ".rg." para puxar o nome do Resource Group corretamente
+  resource_group_name = azurerm_resource_group.rg.name 
 }
 
 resource "azurerm_subnet" "subnet" {
-  name = "subnet-automation"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "subnet-automation"
+  resource_group_name  = azurerm_resource_group.rg.name
+  # CORRIGIDO: Era azurerm_virtual_network, mas o nome do seu recurso acima era azure_virtual_network. Agora ambos usam azurerm
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
-  name = "public-ip-vm"
-  location = azurerm_resource_group.rg.location
+  name                = "public-ip-vm"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Static"
-  sku = "Basic"
+  allocation_method   = "Static"
+  sku                 = "Basic"
 }
 
 resource "azurerm_network_interface" "nic" {
-  name = "nic-vm"
-  location = azurerm_resource_group.rg.location
+  name                = "nic-vm"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name = "internal"
-    subnet_id = azurerm_subnet.subnet.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.public_ip.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
 resource "azurerm_network_security_group" "nsg" {
-    name = "nsg-vm"
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "nsg-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-    security_rule {
-        name = "SSH"
-        priority = 1001
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "22"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-    security_rule {
-        name = "Swagger"
-        priority = 1002
-        direction = "Inbound"
-        access = "Allow"
-        protocol = "Tcp"
-        source_port_range = "*"
-        destination_port_range = "8081"
-        source_address_prefix = "*"
-        destination_address_prefix = "*"
-    }
+  security_rule {
+    name                       = "Swagger"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8081"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id = azurerm_network_interface.nic.id
+  network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name = "vm-automation"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-  size = "Standard_B1s"
-  admin_username = "azureuser"
-  admin_password = var.admin_password
+  name                            = "vm-automation"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = "azureuser"
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
 
-os_disk {
-  caching = "ReadWrite"
-  storage_account_type = "Standard_LRS"
-}
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-source_image_reference {
-  publisher = "Canonical"
-  offer = "0001-com-ubuntu-server-focal"
-  sku = "20_04-lts"
-  version = "latest"
- }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
 }
