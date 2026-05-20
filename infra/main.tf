54888e624c045693e359c77dde8fed713b9c6de9
pauloojr <paulojr7704@outlook.com>
2026-05-20 07:17:34 -0300

    alteração Ubuntu

--- infra/main.tf
+++ infra/main.tf
@@ -110,7 +110,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "20_04-lts-gen2"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 }
